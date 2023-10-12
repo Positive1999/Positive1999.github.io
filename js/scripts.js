ccd91bc5101581f6ed63bb4dcbdcd2e804398626
TVHieu99 <hieutrantran102@gmail.com>
2023-10-12 18:13:46 +0700

    them nut arrow up

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+$(function () {
+    trenCung = $('nav').offset().top;
+     $('.arrow-up').click(function () {
+       $('body,html').animate({ scrollTop:trenCung });
+     })
+   })
